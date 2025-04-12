18db96eacdc4681752baba6cd697b694d83a14be
Muhammadsolih Umarov <muhammadsolih_umarov@epam.com>
2025-04-12 08:26:46 +0500

    Added playwright native DDT

--- src/tests/localization/localization.spec.ts
+++ src/tests/localization/localization.spec.ts
@@ -3,18 +3,13 @@ import { LayoutPage } from '../../pageObject/LayoutPage';
 import { getLocalizationData } from '../../utils/csvReader';
 import { CalculatorPage } from '../../pageObject/CalculatorPage';
 
-test.describe('Localization', () => {
-    const dataSet = getLocalizationData();
+getLocalizationData().forEach(({ languageName, url, headerKeywords, footerKeywords }) => {
+    test(`Verify Header and Footer in ${languageName}`, async ({ page }) => {
+        const calculatorPage = new CalculatorPage(page, url);
+        const layout = new LayoutPage(page);
 
-    for (const { languageName, url, headerKeywords, footerKeywords } of dataSet) {
-        test(`Verify Header/Footer in ${languageName}`, async ({ page }) => {
-            const calculatorPage = new CalculatorPage(page, url);
-            const layout = new LayoutPage(page);
-
-            await calculatorPage.open();
-
-            await layout.expectHeaderContains(headerKeywords);
-            await layout.expectFooterContains(footerKeywords);
-        });
-    }
+        await calculatorPage.open();
+        await layout.expectHeaderContains(headerKeywords);
+        await layout.expectFooterContains(footerKeywords);
+    });
 });
